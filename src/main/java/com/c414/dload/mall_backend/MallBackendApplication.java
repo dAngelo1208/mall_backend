c9a14b5e4e9b199e704ab30b0915d6bca39974ad
Chunyu Yang <yangcy9197@outlook.com>
2022-05-13 15:38:22 +0800

    定时任务

--- src/main/java/com/c414/dload/mall_backend/MallBackendApplication.java
+++ src/main/java/com/c414/dload/mall_backend/MallBackendApplication.java
@@ -2,10 +2,12 @@ package com.c414.dload.mall_backend;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
 import springfox.documentation.oas.annotations.EnableOpenApi;
 
 @SpringBootApplication
 @EnableOpenApi
+@EnableScheduling
 public class MallBackendApplication {
 
     public static void main(String[] args) {
